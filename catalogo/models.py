154bc0ba1bae053a2893ceaf2db743bd9de99138
Arthur <cipitelliarthurgabriel@protonmail.com>
2021-12-14 10:26:57 -0300

    Criação do modelo 'Genero'

--- catalogo/models.py
+++ catalogo/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Genero(models.Model):
+    """Um modelo que representa o gênero do livro"""
+    nome = models.CharField(max_length=200, help_text='Entre com gênero do livro (ex: fantasia).')
+    
+    def __str__(self):
+        """Retorna uma string para representar o modelo"""
+        return self.nome
